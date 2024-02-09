bff8fab02adf60d03695cc0e0346ac877124ec86
Chandan Mohanty <116899402+Cmohanty26@users.noreply.github.com>
2024-02-09 11:38:52 +0530

    Calculator clone

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Calculator from "./components/Calculator";
+
+
+function App() {
+  return (
+    <div >
+    <Calculator/>
+    <h1>Calculator App</h1>
+    </div>
+  );
+}
+
+export default App;
